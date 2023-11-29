bd4cae42106823ea5294b52b0febdb3394b40d48
Victor <victorjv37@gmail.com>
2023-11-29 15:15:57 +0100

    Better styling
    New styles and organization for podcastData.

--- src/components/PodcastData.jsx
+++ src/components/PodcastData.jsx
@@ -13,14 +13,12 @@ const PodcastData = ({ id, podcastId, storagedList }) => {
       {podcastFound && (
         <div className="podcastData">
           <img src={podcastFound.image}></img>
-          <div className="podcastTitleDescription">
-            <h4>{podcastFound.name}</h4>
-            <p>by {podcastFound.artist}</p>
-            <br />
-            <br />
-            <h4>Description:</h4>
-            <p>{podcastFound.description.label}</p>
-          </div>
+          <h4>{podcastFound.name}</h4>
+          <p>by {podcastFound.artist}</p>
+          <h4 id="description">Description:</h4>
+          <p id={podcastFound.description.label.length < 160 ? "shorterP" : "p"}>
+            {podcastFound.description.label}
+          </p>
         </div>
       )}
     </>
